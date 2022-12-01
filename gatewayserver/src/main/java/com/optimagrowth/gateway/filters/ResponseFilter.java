407d514d824af3d1417c041fd833fc842dd2a558
Lenny <61crossroad@gmail.com>
2022-12-02 03:02:18 +0900

    implemented response filter

---
+++ gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -0,0 +1,28 @@
+package com.optimagrowth.gateway.filters;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class ResponseFilter {
+    final FilterUtils filterUtils;
+
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) ->
+            chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+                String correlationId = filterUtils.getCorrelationId(requestHeaders);
+                log.info("Adding the correlation id to the outbound headers. {}", correlationId);
+                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+                log.info("Completing outgoing request for {}.", exchange.getRequest().getURI());
+            }));
+    }
+}
