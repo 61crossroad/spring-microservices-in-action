11cf5fe8a6305943cadd680af9296ab3b01c8aa6
Lenny <61crossroad@gmail.com>
2022-12-02 00:41:26 +0900

    docker-compose done

---
+++ build-for-m1.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+cd configserver
+./mvnw clean package
+cd ..
+
+cd eurekaserver
+./mvnw clean package
+cd ..
+
+cd licensing-service
+./mvnw clean package
+cd ..
+
+cd organization-service
+./mvnw clean package
+cd ..
+
+# mvn clean package
+docker build -t ostock/configserver:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/configserver-0.0.1-SNAPSHOT.jar ./configserver
+docker build -t ostock/eurekaserver:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/eurekaserver-0.0.1-SNAPSHOT.jar ./eurekaserver
+# docker build -t ostock/gatewayserver:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/gatewayserver-0.0.1-SNAPSHOT.jar ./gatewayserver
+docker build -t ostock/licensing-service:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/licensing-service-0.0.1-SNAPSHOT.jar ./licensing-service
+docker build -t ostock/organization-service:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/organization-service-0.0.1-SNAPSHOT.jar ./organization-service
